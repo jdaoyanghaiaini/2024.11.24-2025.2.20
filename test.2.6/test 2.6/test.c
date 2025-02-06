199c73393df655197c46b9f516bd660a5aa31356
jdaoyanghaiaini <2145381080@qq.com>
2025-02-06 20:33:31 +0800

    练习

---
+++ test.2.6/test 2.6/test.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+    for (i = 0; i <= 12; i++)
+    {
+        arr[i] = 0;
+        printf("hello bit\n");
+        printf("hello bit\n");
+    }
+    return 0;
+}
